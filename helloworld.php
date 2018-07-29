7bdb5fd974c2bb3861f03f397f4edef15ffc70ce
tanzil <androjkg@gmail.com>
2018-07-29 13:05:34 +0700

    aplikasi sudah bisa menampilkan hellow world

---
+++ helloworld.php
@@ -0,0 +1,17 @@
+<html>
+  <!-- [START Link] -->
+  <head>
+    <title>PHP runs on GAE<title>
+  </head>
+  <!-- [END csslink] -->
+
+  <body>
+
+    <?php
+    echo "Tes, 1, 2, 3";
+    ?>
+    <h1> PHP is almost coll </h1>
+
+
+  </body>
+</html>
